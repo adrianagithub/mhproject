98d884e80ad3f100daa63ef6d256ad3f0e757cf4
Adriana Cabrera <adi_111@icloud.com>
2022-10-20 22:16:07 -0700

    Added customers and category

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :therapists
   # devise_for :users
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: 'pages#landing_page', :as => 'landing_page'
+  resources :categories
+  resources :customers
+  resources :therapists
   get '/about' => 'pages#about', :as => 'about_page'
   get '/legal_disclaimer' => 'pages#legal_disclaimer'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_230603) do
+ActiveRecord::Schema.define(version: 2022_10_21_050142) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "therapists", force: :cascade do |t|
     t.string "first_name"
@@ -35,6 +51,18 @@ ActiveRecord::Schema.define(version: 2022_10_19_230603) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_views_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
 end
